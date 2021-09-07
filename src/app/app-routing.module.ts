fb02fbc900b05c8690203b0fbce7aeca30b5b7c1
Mehmed Catic <mehmedca@maestralsolutions.com>
2021-09-07 19:52:41 +0200

    Updated UI and header compnent. Made some changes to services, added login feature with observable to control the behaviour when user loggs in and stored user to localStorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './core/_guards/auth.guard';
+import { PreventUnsavedChangesGuard } from './core/_guards/prevent-unsaved-changes.guard';
+
+// const routes: Routes = [
+//   {path: '', component: HomeComponent},
+//   {
+//     path: '',
+//     runGuardsAndResolvers: 'always',
+//     canActivate: [AuthGuard],
+//     children: [
+//       {path: 'movies', component: MovieListComponent},
+//       {path: 'movies/:id', component: MovieDetailComponent},
+//       {path: 'movies/edit', component: MovieEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
+//       {path: 'movies/ratings/:id', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
+//       {path: 'actors', component: ListsComponent},
+//       {path: 'messages', component: MessagesComponent},
+//       {path: 'admin', component: AdminPanelComponent, canActivate: [AdminGuard]},
+//     ]
+//   },
+//   {path: 'not-found', component: NotFoundComponent},
+//   {path: 'server-error', component: ServerErrorComponent},
+//   {path: '**', component: NotFoundComponent, pathMatch: 'full'},
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
